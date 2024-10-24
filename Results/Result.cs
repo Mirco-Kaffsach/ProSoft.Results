0b61d4b7682eadfe75ae24979e97eaa64f4fc7c9
Mirco@Home <mircoswinphone@outlook.com>
2024-10-24 09:24:45 +0200

    feat: Initial ResultPattern

---
+++ Results/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace ProSoft.Results;
+
+/// <summary>
+/// Class Result.
+/// </summary>
+/// <typeparam name="TValue">The type of the t value.</typeparam>
+public class Result<TValue>
+{
+   /// <summary>
+   /// Gets or sets the value.
+   /// </summary>
+   /// <value>The value.</value>
+   public TValue? Value { get; set; }
+
+   /// <summary>
+   /// Gets or sets the type.
+   /// </summary>
+   /// <value>The type.</value>
+   public ResultType Type { get; set; }
+
+   /// <summary>
+   /// Gets or sets the messages.
+   /// </summary>
+   /// <value>The messages.</value>
+   public HashSet<MessageItem> Messages { get; set; } = [];
+}
